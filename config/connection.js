a8a7f97b83cf2fee39c3e5f40bedd2f365eb5f3c
unknown <44147809+LSabin23@users.noreply.github.com>
2022-01-28 21:06:18 -0500

    updated (downgraded?) dotenv version to correct password issue

--- config/connection.js
+++ config/connection.js
@@ -6,6 +6,11 @@ const Sequelize = require('sequelize')
 // create connection to our database, pass in your MySQL information for username and password
 let sequelize
 
+/*
+  Was receiving an ACCESS DENIED error in this project when using credentials that worked on other projects. Had to console log these env variables to see what sequelize was seeing as credentials and discovered dotenv version 14+ was ignoring a character so switched it to dotenv version 10.0.0.
+*/
+// console.log(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW)
+
 if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL)
 } else {
