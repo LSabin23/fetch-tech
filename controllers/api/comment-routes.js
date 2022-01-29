f45150ee2846abc958c2cc781b69750ec0be1c4c
unknown <44147809+LSabin23@users.noreply.github.com>
2022-01-29 14:51:08 -0500

    added comments functionality

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -1,14 +1,47 @@
 const router = require('express').Router()
 const { Comment } = require('../../models')
-// const withAuth = require('../../utils/auth')
+const withAuth = require('../../utils/auth')
 
 // GET all comments
 router.get('/', (req, res) => {
-  console.log('Called GET all comments route.')
+  Comment.findAll({
+    attributes: [
+      'id',
+      'comment_text',
+      'user_id',
+      'post_id'
+    ]
+  })
+    .then(dbCommentData => res.json(dbCommentData))
+    .catch(err => {
+      console.log(err)
+      res.status(500).json(err)
+    })
 })
 
 // POST /
 // create comment
+// authguard
+router.post('/', withAuth, (req, res) => {
+  // check the session
+  if (req.session) {
+    Comment.create({
+      comment_text: req.body.comment_text,
+      post_id: req.body.post_id,
+      // use the id from the session
+      user_id: req.session.user_id
+    })
+      .then(dbCommentData => res.json(dbCommentData))
+      .catch(err => {
+        console.log(err)
+        res.status(400).json(err)
+      })
+  }
+})
+
+// PLACEHOLDER
+// DELETE /:id
+// delete comments
 // authguard
 
 module.exports = router
